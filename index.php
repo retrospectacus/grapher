579701b761b2836885450a813c5300a70fb7facd
Retrospectacus <retrospectacus@gmail.com>
2020-01-26 00:46:38 -0800

    support hours and dates

--- index.php
+++ index.php
@@ -1,13 +1,72 @@
 <?php
-$d = 1;
+$debug = 0;
+$denominator = 1;
+$denominator_cutoff = 10; // # of days to graph before scaling down
+$current = "Current";
+
+function files_from_find_command($args) {
+  global $debug;
+  if ($debug) {
+    error_log(">>> find /home/adama/retro-house/ -type f -name '*Enviroboard.csv' ${args} -printf \"%T@ %p\n\" 2>/dev/null | sort -n</pre>");
+  }
+  return `find /home/adama/retro-house/ -type f -name '*Enviroboard.csv' ${args} -printf "%T@ %p\n" 2>/dev/null | sort -n `;
+}
+
+function days_ago($days) {
+  return "-mtime -${days}";
+}
+
+function hours_ago($hours) {
+  $mins = $hours * 60;
+  return "-mmin -${mins}";
+}
+
+function date_range($fromU, $toU = false) {  // Unix
+  if (!$toU) {
+    $toU = $fromU + 86400;
+  }
+  $from = strftime('%F', $fromU);
+  $to = strftime('%F', $toU);
+  return "-newermt ${from} ! -newermt ${to}";
+}
+
 if (array_key_exists('d', $_GET)) {
-  $d = intval($_GET['d']);
+  $d = $_GET['d'];
+  $dn = 0;
+  if (is_numeric($d)) {
+    $dn = intval($_GET['d']);
+  }
+  $dat = strtotime($_GET['d']);
+  
+  if (substr($d, -1) == 'h' && is_numeric(substr($d, 0, -1))) {
+    $hn = intval(substr($d, 0, -1));
+    if ($hn >= 1) {
+      $files = files_from_find_command(hours_ago($hn));
+      $denominator = floor($hn / 24 / $denominator_cutoff) + 1;
+    }
+    else {
+      $d = 1;
+      $files = files_from_find_command(days_ago(1));
+    }
+  }
+  else if ($dn >= 1) {
+    $files = files_from_find_command(days_ago($dn));
+    $denominator = floor($dn / $denominator_cutoff) + 1;
+  }
+  else if ($dat) {
+    $files = files_from_find_command(date_range($dat));
+    $current = "";
+  }
+  else {
+    $d = 1;
+    $files = files_from_find_command(days_ago(1));
+  }
 }
-if (!$d || $d < 1) {
+else {
   $d = 1;
+  $files = files_from_find_command(days_ago(1));
+// $title = "1 Day Plot";
 }
-$denominator = floor($d / 30) + 1;
-$files = `find /home/adama/retro-house/ -type f -name '*Enviroboard.csv' -mtime -$d -printf "%T@ %p\n" 2>/dev/null | sort -n `;
 
 $files = explode("\n", $files);
 
@@ -67,39 +126,57 @@ include('header.html');
 echo "
 <small style='position:absolute;z-index:1'>
 <form>
- Graph <input name='d' value='$d' size='3'/> days <input type='submit' value='Go'/>
+ Graph <input name='d' value='$d' size='5'/> <input type='submit' value='Go'/>
 </form>";
 
 if ($denominator > 1) {
-  echo "<span style='color:red'>Warning: Large data set requested. Resolution has been reduced to $denominator-minute intervals.</span><br>";
+  echo "<span style='color:red'>
+    Warning: Large data set requested. 
+    Resolution has been reduced to ${denominator}-minute intervals. 
+    You can also enter a single date like 2019-11-20 or \"Last Tuesday\".
+    </span><br>";
 }
 
-echo "
-Current Readings at $datetime<br/>";
-foreach ($headers as $i => $key) {
-  if (array_key_exists($key, $mapping) && m($key) !== 'Zero') {
-    echo ' | '.m($key).' = '.substr($line[$i],0,strpos($line[$i], '.')+3) . '°C';
+if (!$data) {
+  echo "<span style='color:red'>
+    Warning: No Data found for \"". addslashes($d) ."\". 
+    Enter a date in the past, a number of days, or a number with 'h' for hours.</span><br>";
+}
+
+function ft($v) { // format temp
+  return substr($v,0,strpos($v, '.')+3) . '°C';
+}
+
+if ($data) {
+  echo "
+  <big>". ft(min($line[10], $line[11])) ." /". ft(max($line[10], $line[11])) ." Outside ". ($dat? addslashes($d): '') ."</big>
+  <br>${current} Readings at $datetime<br/>";
+  foreach ($headers as $i => $key) {
+    if (array_key_exists($key, $mapping) && m($key) !== 'Zero') {
+      echo ' | '.m($key).' = '.ft($line[$i]);
+    }
   }
 }
 echo "<br><a target='_blank' href='https://weather.gc.ca/city/pages/yt-16_metric_e.html'>Weather from Environment Canada</a>";
 echo "</small>";
 
-include('plot_lib.html');
-// $title = "$d Day Plot";
-echo "[";
-$first = true;
-foreach($mapping as $label) {
-  if (!$first) {
-    echo ",";
+if ($data) {
+  include('plot_lib.html');
+  echo "[";
+  $first = true;
+  foreach($mapping as $label) {
+    if (!$first) {
+      echo ",";
+    }
+    else {
+      $first = false;
+    }
+    echo '{"mode":"lines","name":"'.$label.'","type":"scatter",' .
+      '"x":["'.implode('","',array_keys($data[$label])).'"],' .
+      '"y":['.implode(',',$data[$label]).']}';
   }
-  else {
-    $first = false;
-  }
-  echo '{"mode":"lines","name":"'.$label.'","type":"scatter",' .
-    '"x":["'.implode('","',array_keys($data[$label])).'"],' .
-    '"y":['.implode(',',$data[$label]).']}';
+  echo "],";
+  include('template_line.html');
 }
-echo "],";
-include('template_line.html');
 include('footer.html');
 ?>
